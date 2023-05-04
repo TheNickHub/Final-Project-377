508308a9362e69d7d2e1137b9a600b4410713525
TheNickHub <shenn18@terpmail.umd.edu>
2023-05-04 19:18:14 -0400

    uncomment

--- script.js
+++ script.js
@@ -60,26 +60,26 @@ function markerPlace(array, map) {
 }
 
 async function mainEvent() {
-  // // the async keyword means we can make API requests
-  // const mainForm = document.querySelector(".main_form");
-  // const loadDataButton = document.querySelector("#data_load");
-  // const clearDataButton = document.querySelector("#data_clear");
-  // const generateListButton = document.querySelector("#generate");
-  // const textField = document.querySelector("#hosp");
+  // the async keyword means we can make API requests
+  const mainForm = document.querySelector(".main_form");
+  const loadDataButton = document.querySelector("#data_load");
+  const clearDataButton = document.querySelector("#data_clear");
+  const generateListButton = document.querySelector("#generate");
+  const textField = document.querySelector("#hosp");
 
-  // //const loadAnimation = document.querySelector("#data_load_animation");
-  // //loadAnimation.style.display = "none";
-  // //generateListButton.classList.add("hidden");
+  //const loadAnimation = document.querySelector("#data_load_animation");
+  //loadAnimation.style.display = "none";
+  //generateListButton.classList.add("hidden");
 
-  // const carto = initMap();
+  const carto = initMap();
 
-  // const storedData = localStorage.getItem("storedData");
-  // let parsedData = JSON.parse(storedData);
-  // if (parsedData?.length > 0) {
-  //   generateListButton.classList.remove("hidden");
-  // }
+  const storedData = localStorage.getItem("storedData");
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) {
+    generateListButton.classList.remove("hidden");
+  }
 
-  // let currentList = [];
+  let currentList = [];
   const results = await fetch(
     "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
   );
@@ -87,110 +87,111 @@ async function mainEvent() {
   // This changes the response from the GET into data we can use - an "object"
   const storedList = await results.json();
   // /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  // loadDataButton.addEventListener("click", async (submitEvent) => {
-  //   // async has to be declared on every function that needs to "await" something
-  //   console.log("Loading Data");
-  //   loadAnimation.style.display = "inline-block";
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    // async has to be declared on every function that needs to "await" something
+    console.log("Loading Data");
+    loadAnimation.style.display = "inline-block";
 
   //   // Basic GET request - this replaces the form Action
-  //   const results = await fetch(
-  //     "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
-  //   );
+    const results = await fetch(
+      "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
+    );
 
-  //   // This changes the response from the GET into data we can use - an "object"
-  //   const storedList = await results.json();
-  //   localStorage.setItem("storedData", JSON.stringify(storedList));
-  //   parsedData = storedList;
+    // This changes the response from the GET into data we can use - an "object"
+    const storedList = await results.json();
+    localStorage.setItem("storedData", JSON.stringify(storedList));
+    parsedData = storedList;
 
-  //   if (parsedData?.length > 0) {
-  //     generateListButton.classList.remove("hidden");
-  //   }
+    if (parsedData?.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
 
-  //   loadAnimation.style.display = "none";
-  //   // console.table(storedList);
-  // });
+    loadAnimation.style.display = "none";
+    // console.table(storedList);
+  });
 
-  // generateListButton.addEventListener("click", (event) => {
-  //   console.log("generate new list");
-  //   currentList = cutHospitalList(parsedData);
-  //   console.log(currentList);
-  //   injectHTML(currentList);
-  //   markerPlace(currentList, carto);
-  // });
+  generateListButton.addEventListener("click", (event) => {
+    console.log("generate new list");
+    currentList = cutHospitalList(parsedData);
+    console.log(currentList);
+    injectHTML(currentList);
+    markerPlace(currentList, carto);
+  });
 
-  // textField.addEventListener("input", (event) => {
-  //   console.log("input", event.target.value);
-  //   const newList = filterList(currentList, event.target.value);
-  //   console.log(newList);
-  //   injectHTML(newList);
-  //   markerPlace(newList, carto);
-  // });
+  textField.addEventListener("input", (event) => {
+    console.log("input", event.target.value);
+    const newList = filterList(currentList, event.target.value);
+    console.log(newList);
+    injectHTML(newList);
+    markerPlace(newList, carto);
+  });
 
-  // clearDataButton.addEventListener("click", (event) => {
-  //   console.log("clear browser data");
-  //   localStorage.clear();
-  //   console.log("localStorage Check", localStorage.getItem("storedData"));
-  // });
-  // let hospitals = new Set() 
-  
-  // function getHospitals() {
-  //   // console.log(storedList)
-  //   storedList.forEach(hospital => {
-  //     hospitals.add(hospital.city)
-  //   })
-  // }
+  clearDataButton.addEventListener("click", (event) => {
+    console.log("clear browser data");
+    localStorage.clear();
+    console.log("localStorage Check", localStorage.getItem("storedData"));
+  });
+  let hospitals = new Set();
+
+  function getHospitals() {
+    console.log(storedList)
+    storedList.forEach((hospital) => {
+      hospitals.add(hospital.city);
+    });
+  }
+  function filterCity(city) {
+    const matched = [];
+    storedList.forEach(hospital);
+    return matched;
+  }
+  console.log(hospitals);
+  let hospitalOptions = "";
+  Array.from(hospitals).forEach((city) => {
+    hospitalOptions += `<option value="${city}">${city}</option>`;
+  });
+  console.log(hospitalOptions);
+  let hospital = new Set();
+
+  function getHospitals() {
+    storedList.forEach((hospital) => {
+      hospitals.add(hospital.city);
+    });
+  }
+
   // function filterCity(city) {
   // const matched = []
   // storedList.forEach(hospital)
-  // return matched 
+  // return matched
   // }
-  // console.log(hospitals)
-  // let hospitalOptions = ""
-  // Array.from(hospitals).forEach(city => {
-  //   hospitalOptions += `<option value="${city}">${city}</option>`
 
-  // })
-  // console.log(hospitalOptions)
-  // let hospital = new Set()
+  function filterCity(city) {
+    const matched = [];
+    storedList.forEach((hospital) => {
+      if (hospital.city.toLowerCase() === city.toLowerCase()) {
+        matched.push(hospital);
+      }
+    });
+    return matched;
+  }
 
-  
-  // function getHospitals() {
-  //   storedList.forEach(hospital => {
-  //     hospitals.add(hospital.city)
-  //   })
-  // }
-  
-  // function filterCity(city) {
-  // const matched = []
-  // storedList.forEach(hospital)
-  // return matched 
-  // }
-  
-  getHospitals()
-  createDropdown()
-  
-  document.getElementById("city-dropdown").addEventListener("change", (event) => {
-    const selectedCity = event.target.value
-    const filteredList = filterCity(selectedCity)
-    console.log(filteredList)
-    injectHTML(filteredList)
-    markerPlace(filteredList, carto)
-  })
-  
+  getHospitals();
+  createDropdown();
 
+  document
+    .getElementById("city-dropdown")
+    .addEventListener("change", (event) => {
+      const selectedCity = event.target.value;
+      const filteredList = filterCity(selectedCity);
+      console.log(filteredList);
+      injectHTML(filteredList);
+      markerPlace(filteredList, filter);
+    });
 
+  // Get value from dropdown input using an eventlistener
+  // Use value to filter stored list
+  //
 
-// Get value from dropdown input using an eventlistener 
-// Use value to filter stored list 
-// 
-
-  document.querySelector('#filter').innerHTML = hospitalOptions
-
-
-
-
-
-
+  document.querySelector("#filter").innerHTML = hospitalOptions;
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
